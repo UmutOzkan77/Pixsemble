52940eb3cd8fbecb0b1c709c44d08df8f0b49c67
UmutOzkan77 <mehmetumut2005@gmail.com>
2026-01-25 15:33:37 +0300

    fix: Set default proxy URL in Storage fallback

--- js/storage.js
+++ js/storage.js
@@ -114,7 +114,7 @@ const Storage = {
     },
 
     getProxyUrl() {
-        return this.get(this.KEYS.PROXY_URL, '');
+        return this.get(this.KEYS.PROXY_URL, 'https://pixsemble2.mehmetumut2005.workers.dev');
     },
 
     setProxyUrl(url) {
